28713790c8644575c8bd4a6b3e2e5fe94ac34c08
Michael Park <tjp4ca@gmail.com>
2022-10-19 17:35:56 -0400

    login css and merged kenny/develop

--- client/src/pages/PageNotFound.js
+++ client/src/pages/PageNotFound.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const PageNotFound = () => {
-    return <h1>404</h1>;
+    return (
+        <div className='noPage'>
+            <h1>404</h1>
+            <h2>Page Not Found</h2>
+        </div>
+
+    )
+    
 }
 
 export default PageNotFound;
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,9 +1,70 @@
 import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
+import { Link } from 'react-router-dom';
 
 // import auth
 
 const Signup = () => {
+    const [formInput, setFormInput] = useState({ 
+        email: '',
+        username:'', 
+        password:'' 
+    });
+
+    const handleChange = (event) => {
+        console.log(event.target.value);
+
+        const { name, value } = event.target;
+
+        setFormInput({
+            ...formInput,
+            [name]: value,
+        });
+    };
+
+    const handleClickSignupBtn = async (event) => {
+        event.preventDefault();
+
+        console.log('clicked signup');
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleClickSignupBtn}>
+                {/* <div>
+                    <div><Link to="/login" className='tabBtnLink'>Login</Link></div>
+                    <div><Link to="/signup" className='tabBtnLink'>Signup</Link></div>
+                </div> */}
+
+
+                {/* <h2>Welcome {formInput.username}</h2> */}
+                
+                <input 
+                    onChange={handleChange}
+                    name='username'
+                    type='text'
+                    placeholder='Username'
+                    value={formInput.username}
+                />
+                <input 
+                    onChange={handleChange}
+                    name='email'
+                    type='email'
+                    placeholder='Email'
+                    value={formInput.email}
+                />
+                <input 
+                    onChange={handleChange}
+                    name='password'
+                    type='password'
+                    placeholder='Password'
+                    value={formInput.password}
+                />
+                <button type='submit'><span>Signup</span></button>
+            </form>
+        </div>
+
+    )
 
 };
 
