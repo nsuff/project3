a8abf6a22beeb155331750555c6c6ce775237c70
Nino Suffoletta <nbsuffoletta@gmail.com>
2022-10-20 16:22:39 -0400

    more stuff

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import './Login.css';
 import './Signup.css';
+import './Home.css';
 
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,15 +4,22 @@ import React from "react";
 // import Cart from "../components/Cart";
 
 const Home = () => {
+
+  const documentWidth = document.body.clientWidth;
+  const documentWidthString = documentWidth.toString() + 'px';
+  const fullWidth = {
+    width: '3000px'
+  }
+
   return (
-    <div className="container-fluid border border-success">
+    <div className="container border border-success mx-0 px-0 justify-content-left home-box" style={fullWidth}>
       {/*interior divs can be moved into componants at a later date */}
       {/* <CategoryMenu />
       <ProductList />
       <Cart /> */}
-      <div>
+      <div className="row mx-0 px-0">
         <div className="col-lg-3 border border-warning text-white">
-          <h1>left column</h1>
+          <h1>left column {documentWidthString}</h1>
         </div>
         <div className="col-lg-6 border border-danger text-white">
           <h1>middle column</h1>
